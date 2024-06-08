9ba6a9a155c65516ca576ceef23ed16ba197d40a
YiCChi <syanxk@gmail.com>
2024-06-09 00:49:43 +0900

    build: rm lint-staged

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-const lintStagedConfig = {
-  // Type check TypeScript files
-  // Lint then format TypeScript files
-  '**/*.(ts|tsx)': (fileNames) => [`pnpm exec biome format --write ${fileNames.join(' ')}`],
-};
-
-export default lintStagedConfig;
