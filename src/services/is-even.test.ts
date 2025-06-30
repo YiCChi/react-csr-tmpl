57931a71b9a63d288b59709e20e62949b6769659
YiCChi <syanxk@gmail.com>
2025-07-01 00:04:30 +0900

    build: update deps and add rstest

--- src/services/is-even.test.ts
+++ src/services/is-even.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test } from '@rstest/core';
 import { isEven } from './is-even.ts';
 
 describe.concurrent('isEven', () => {
